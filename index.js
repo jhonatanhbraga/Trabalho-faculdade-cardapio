76af14a43938e1d2e0434a91d66352c5007b7298
Jhonatan Hilario <jhonatanbraga2404@gmail.com>
2025-04-01 22:40:44 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let total = 0;// Valor total do carrinho
+const itensCarrinho = [];// Array para armazenar os itens do carrinho
+function adicionarAoCarrinho (preco){   
+    total += preco;
+
+itensCarrinho.push(preco);//add ao carrinho
+document.getElementById("total").textContent = total.toFixed(2);
+//mostra novo total 
+}
+atualizarCarrinho();
+//atualiza a lista de itens no carrinho
+
+function atualizarCarrinho (){
+    const listaItens= document.getElementById('itensCarrinho');
+    listaItens.innerHTML = '';//limpa lista atual 
+
+    //adiciona itens ao carrinho
+    itensCarrinho.forEach((item,index) => {
+        const li =document.createElement('li');
+        li.textContent= `produto ${index + 1} - R$ ${item.toFixed(2)}`
+        listaItens.appendChild(li);
+
+
+    })
+}
